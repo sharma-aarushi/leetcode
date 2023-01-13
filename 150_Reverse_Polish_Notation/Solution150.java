bb630cbe5af8f7497d5e874313ae81baf7159a8c
Aarushi Sharma <s.aarushi@aol.com>
2023-01-13 23:09:41 +0400

    Solve leetcode question 150

---
+++ 150_Reverse_Polish_Notation/Solution150.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class Solution150 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                isOperator(tokens[i], stack);
+
+            } else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+
+    }
+
+    private static void isOperator(String s, Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            switch (s) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-":
+                    int latest_val = stack.pop();
+                    int earlier_val = stack.pop();
+                    stack.push(earlier_val - latest_val);
+                    break;
+
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/":
+                    try {
+                        int val1 = stack.pop();
+                        int val2 = stack.pop();
+                        stack.push(val2 / val1);
+                        break;
+                    } catch (ArithmeticException e) {
+                        System.out.println("Cannot divide a number by 0");
+                    }
+
+            }
+        }
+    }
+}
