2ab4d793dae9fdd285699008751b4fc3bf652afe
Aarushi Sharma <s.aarushi@aol.com>
2023-03-15 07:40:55 +0400

    Solve leetcode question 21

---
+++ 21_Merge_linked_lists/Solution21.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        return mergeHelper(list1, list2);
+    }
+
+    private ListNode mergeHelper(ListNode list1, ListNode list2) {
+        ListNode newHead = new ListNode(0);
+        ListNode curr = newHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else {
+            curr.next = list2;
+        }
+        return newHead.next;
+    }
+}
